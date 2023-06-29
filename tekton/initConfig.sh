cb084e0f887be0e2e58a51d5e560be4fd67cb9a2
phaithoona <31614756+phaithoona@users.noreply.github.com>
2023-06-29 20:02:24 +0700

    Update initConfig.sh
    update

--- tekton/initConfig.sh
+++ tekton/initConfig.sh
@@ -7,10 +7,10 @@
 # User name to access the common services 
 COMMON_SERVICES_USERNAME=admin
 # Credentials to access the common services 
-COMMON_SERVICES_PASSWORD=xxx
+COMMON_SERVICES_PASSWORD=TLZwA4Smz47Zp1IXC56uoosANT0ELKjo
 # End point of the common services 
 # Output of --> oc get routes -n kube-system | grep icp-console |  awk -F' ' '{print $2 }'
-COMMON_SERVICE_ENDPOINT=icp-console.xxx
+COMMON_SERVICE_ENDPOINT=cpd-cp4i.mycluster-jp-tok-2-880707-b9b2c5563a22400d14b20ef751ace8d5-0000.jp-tok.containers.appdomain.cloud
 
 # cloudType is set to ibmcloud or onprem
 CLOUD_TYPE=ibmcloud
@@ -19,15 +19,15 @@ CLOUD_TYPE=ibmcloud
 OFFLINE_INSTALL=false
 
 # fileStorage is set to "nfs" or "csi-cephfs" or "ibmc-file-gold" or "any RWX storage provider"
-FILE_STORAGE=ibmc-file-gold
+FILE_STORAGE=integration-storage
 
 # blockStorage is set to "rook-ceph-block" or "ibmc-block-gold" or ""any RWO storage provider"
-BLOCK_STORAGE=ibmc-block-gold
+BLOCK_STORAGE=ibmc-vpc-block-general-purpose
 
 # Credentials to access git repository
 # API Key can be created using the link --> https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line
-GIT_USER_NAME=xxx
-GIT_API_KEY_OR_PASSWORD=xxx
+GIT_USER_NAME=phaithoona
+GIT_API_KEY_OR_PASSWORD=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
 
 # Credentials to access OpenShift Cluster
 OPENSHIFT_CLUSTER_USERNAME=xxx
