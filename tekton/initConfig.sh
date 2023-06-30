d7236e7945e74c725f55557b4e623105b11ccdab
phaithoona <31614756+phaithoona@users.noreply.github.com>
2023-06-30 10:55:45 +0700

    Update initConfig.sh

--- tekton/initConfig.sh
+++ tekton/initConfig.sh
@@ -7,7 +7,7 @@
 # User name to access the common services 
 COMMON_SERVICES_USERNAME=admin
 # Credentials to access the common services 
-COMMON_SERVICES_PASSWORD=xxx
+COMMON_SERVICES_PASSWORD=TLZwA4Smz47Zp1IXC56uoosANT0ELKjo
 # End point of the common services 
 # Output of --> oc get routes -n kube-system | grep icp-console |  awk -F' ' '{print $2 }'
 COMMON_SERVICE_ENDPOINT=cpd-cp4i.mycluster-jp-tok-2-880707-b9b2c5563a22400d14b20ef751ace8d5-0000.jp-tok.containers.appdomain.cloud
@@ -27,11 +27,11 @@ BLOCK_STORAGE=ibmc-vpc-block-general-purpose
 # Credentials to access git repository
 # API Key can be created using the link --> https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line
 GIT_USER_NAME=phaithoona
-GIT_API_KEY_OR_PASSWORD=xxx
+GIT_API_KEY_OR_PASSWORD=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
 
 # Credentials to access OpenShift Cluster
-OPENSHIFT_CLUSTER_USERNAME=xxx
-OPENSHIFT_CLUSTER_PASSWORD=xxx
+OPENSHIFT_CLUSTER_USERNAME=apikey
+OPENSHIFT_CLUSTER_PASSWORD=ZD5Wwux067eD7xc9MiMdPhSRmdJdLYU5g6uuVTPIdeFa
 
 #
 # End of configuration
